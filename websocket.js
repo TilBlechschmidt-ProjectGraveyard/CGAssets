95ba1809da01bf97281b4439745f00847abeff81
Noah Peeters <noahpeeters@Noahs-MBP-WLAN.fritz.box>
2017-06-28 20:03:30 +0200

    Fixed clock pause

--- websocket.js
+++ websocket.js
@@ -1,14 +1,25 @@
+var hidden = false;
+
+var variables = {
+    startTime: 0,
+    pauseTime: 0
+}
+
+
+var isController = window.location.pathname.indexOf("control.html") > 0
+
 var ws = new WebSocket("ws://" + window.location.hostname + ":8080");
 
-var hidden = false;
 
 ws.onmessage = function (data) {
     data = data.data;
     console.log(data);
     if (typeof data === 'string') {
         data = JSON.parse(data);
+        
+        
 
-        if (data.type === "goto") {
+        if (!isController && data.type === "goto") {
             if (!hidden && typeof window["hide"] === "function") {
                 window["hide"]();
                 setTimeout(function () {
@@ -18,21 +29,19 @@ ws.onmessage = function (data) {
                 window.location = data.target;
             }
             // window.location = data.target;
-        } else if (data.type === "func") {
+        } else if (isController && data.type === "func") {
             if (typeof window[data.name] === "function") {
                 window[data.name]();
 
                 hidden = data.name === "hide";
             }
         } else if (data.type === "var") {
+            variables[data.name] = data.value
+            
             if (data.name === "score1") {
                 document.getElementById("counter").setAttribute("data-team1", data.value)
             } else if (data.name === "score2") {
                 document.getElementById("counter").setAttribute("data-team2", data.value)
-            } else if (data.name === "startTime") {
-                window["startTime"] = data.value;
-            } else if (data.name === "pauseTime") {
-                window["pauseTime"] = data.value;
             }
         }
     }
