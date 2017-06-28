1798926376624705bca68be1dc13c52e76170573
Til Blechschmidt <til@blechschmidt.de>
2017-06-28 18:40:31 +0200

    Added control functionality - Noah

--- server.js
+++ server.js
@@ -11,13 +11,41 @@ wss.broadcast = function broadcast(data) {
     });
 };
 
+var variables = {
+    score1: 0,
+    score2: 0
+};
+
 wss.on('connection', function connection(ws) {
+
+    for (var key in variables) {
+        ws.send(JSON.stringify({ type: "var", name: key, value: variables[key] }))
+    }
+
+
+
     ws.on('message', function incoming(data) {
         console.log(data);
         // Broadcast to everyone else.
+        var message = data;
+
+        var pdata = JSON.parse(data);
+
+        if (pdata.type === "var") {
+            var name = pdata.name;
+
+            if (pdata.opp === "inc") {
+                variables[name]++;
+            } else if (pdata.opp === "dec") {
+                variables[name]--;
+            }
+
+            message = JSON.stringify({ type: "var", name: name, value: variables[name] })
+        }
+
         wss.clients.forEach(function each(client) {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(data);
+                client.send(message);
             }
         });
     });
--- websocket.js
+++ websocket.js
@@ -1,4 +1,6 @@
-var ws = new WebSocket("ws://127.0.0.1:8080");
+var ws = new WebSocket("ws://" + window.location.hostname + ":8080");
+
+var hidden = false;
 
 ws.onmessage = function (data) {
     data = data.data;
@@ -7,7 +9,27 @@ ws.onmessage = function (data) {
         data = JSON.parse(data);
 
         if (data.type === "goto") {
-            window.location = data.target;
+            if (!hidden && typeof window["hide"] === "function") {
+                window["hide"]();
+                setTimeout(function () {
+                    window.location = data.target;
+                }, 2000)
+            } else {
+                window.location = data.target;
+            }
+            // window.location = data.target;
+        } else if (data.type === "func") {
+            if (typeof window[data.name] === "function") {
+                window[data.name]();
+
+                hidden = data.name === "hide";
+            }
+        } else if (data.type === "var") {
+            if (data.name === "score1") {
+                document.getElementById("counter").setAttribute("data-team1", data.value)
+            } else if (data.name === "score2") {
+                document.getElementById("counter").setAttribute("data-team2", data.value)
+            }
         }
     }
 };
